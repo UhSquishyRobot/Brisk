a8b7d485b31564cc497fbb2b39d1c7cfbc4d10d8
brendan schwarz <bschwarz890@gmail.com>
2020-02-23 17:44:16 -0800

    include mpc library and expand REPL to include its functions

--- prompt.c
+++ prompt.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include "mpc.h"
 
 static char input[2048];
 
 int main(int argc, char** argv) {
+	/* Create parsers */
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Brisk = mpc_new("brisk");
+
+	/* Define with language */
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"							\
+		number	: /-?[0-9]+/;					\
+		operator: '+' | '-' | '*' | '/';			\
+		expr	: <number> | '(' <operator> <expr>+ ')';	\
+		brisk	: /^/ <operator> <expr>+ /$/;			\
+		",
+	Number, Operator, Expr, Brisk 
+	);
+
 	puts("Brisk Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 
@@ -10,8 +28,17 @@ int main(int argc, char** argv) {
 		fputs("brisk> ", stdout);
 		fflush(stdout);
 		fgets(input, 2048, stdin);
-		printf("no you're a %s", input);
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Brisk, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 	}
+
+	mpc_cleanup(4, Number, Operator, Expr, Brisk);
 	
 	return 0;
 }
