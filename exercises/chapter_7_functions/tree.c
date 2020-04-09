d6195c024fcc14795498f4e765370b681fa2b7dc
brendan schwarz <bschwarz890@gmail.com>
2020-04-08 20:07:53 -0700

    create tree type struct and write functions that compute # of leaves and # of branches

---
+++ exercises/chapter_7_functions/tree.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int contents;
+    struct Node* leftChild;
+    struct Node* rightSibling;
+} Node;
+
+int count_leaves(struct Node* head) {
+    int result = 0;
+
+    if (head->leftChild == NULL) {
+       result += 1;
+    }
+    else {
+        result += count_leaves(head->leftChild);
+    }
+
+    if (head->rightSibling != NULL) {
+        result += count_leaves(head->rightSibling);
+    }
+
+    return result;
+}
+
+int count_branches(struct Node* n) {
+    int result = 0;
+
+    if (n->leftChild != NULL) {
+        result = 1 + count_branches(n->leftChild);
+    }
+
+    if (n->rightSibling != NULL) {
+        result = result + 1 + count_branches(n->rightSibling);
+    }
+
+    return result;
+}
+
+/*
+ *          A(5)
+ *      B(7)   C(12)
+ *   D(1)         F(4)
+ *  E(9)        G(10) H(9) I(8)
+ *
+ * */
+
+
+int main() {
+    struct Node* a;
+    struct Node* b;
+    struct Node* c;
+    struct Node* d;
+    struct Node* e;
+    struct Node* f;
+    struct Node* g;
+    struct Node* h;
+    struct Node* i;
+
+    a = (struct Node*)malloc(sizeof(struct Node));
+    b = (struct Node*)malloc(sizeof(struct Node));
+    c = (struct Node*)malloc(sizeof(struct Node));
+    d = (struct Node*)malloc(sizeof(struct Node));
+    e = (struct Node*)malloc(sizeof(struct Node));
+    f = (struct Node*)malloc(sizeof(struct Node));
+    g = (struct Node*)malloc(sizeof(struct Node));
+    h = (struct Node*)malloc(sizeof(struct Node));
+    i = (struct Node*)malloc(sizeof(struct Node));
+    
+    a->leftChild = b;
+    a->rightSibling = NULL;
+
+    b->leftChild = d;
+    b->rightSibling = c;
+
+    c->leftChild = f;
+    c->rightSibling = NULL;
+
+    d->leftChild = e;
+    d->rightSibling = NULL;
+
+    e->leftChild = NULL;
+    e->rightSibling = NULL;
+
+    f->leftChild = g;
+    f->rightSibling = NULL;
+
+    g->leftChild = NULL;
+    g->rightSibling = h;
+
+    h->leftChild = NULL;
+    h->rightSibling = i;
+
+    i->leftChild = NULL;
+    i->rightSibling = NULL;
+
+    int res = count_branches(a);
+
+    printf("%d\n", res);
+
+    return 0;
+}
